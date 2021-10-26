edf71f7b9ef159bb55f70084307f10bed55969ec
Seth Hollingsead <Iceversaka@hotmail.com>
2021-10-26 15:13:52 -0500

    advanced-configuration: Start automated version control and automated documentation.

---
+++ src/executrix/prompt-sync.js
@@ -0,0 +1,12 @@
+/**
+ * @file prompt-sync.js
+ * @module prompt-sync
+ * @description A synchronous prompt for nodeJS.
+ * @requires {@link https://www.npmjs.com/package/fs|fs}
+ * @requires {@link https://www.npmjs.com/package/strip-ansi|strip-ansi}
+ * @author heapwolf
+ * @editor Seth Hollingsead
+ * @date 2021/10/26
+ * @copyright Copyright © 2021-… by Seth Hollingsead. All rights reserved
+ * @reference https://github.com/heapwolf/prompt-sync/blob/master/index.js
+ */
