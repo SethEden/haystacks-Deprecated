ce450a9e68a5bd87e7c3611bb278b4483fce14f1
Seth Hollingsead <Iceversaka@hotmail.com>
2022-03-12 23:25:10 -0600

    Build: Start to flesh out the very beginning of the buildRelease application code base.

---
+++ build/buildRelease/src/buildRelease.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * @file buildRelease.js
+ * @module buildRelease
+ * @description This is the main init for the buildRelease application.
+ * It contains everything to build and deploy the entire npm package.
+ * @author Seth Hollingsead
+ * @date 2022/03/12
+ * @copyright Copyright © 2022-… by Seth Hollingsead. All rights reserved
+ */
